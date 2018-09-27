b08105b5f964f7f2f6aafa1597e9e12cba4ba07d
inoDevlog <ino0319@gmail.com>
2018-09-28 00:45:10 +0900

    no message

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 Ext.onReady(function(){
+	//layout2 : 'center', 'absolute', 'accordion', 'card', 'hbox/vbox'
 	Ext.create("Ext.container.Viewport",{
+		layout : "fit",
+		border : true,
+		items :
+		[
+		 	{
+		 		xtype : 'panel',
+		 		title : '부모패널',
+		 		layout : "accordion",
+		 		items :
+		 		[
+		 		 	{
+		 		 		xtype : 'panel',
+		 		 		width : 300,
+		 		 		height : 300,
+		 		 		x : 100,
+		 		 		y : 200,
+		 		 		border : true,
+		 		 		title : '첫째 패널'
+
+		 		 	},
+		 		 	{
+		 		 		xtype : 'panel',
+		 		 		x : 150,
+		 		 		y : 300,
+		 		 		width : 300,
+		 		 		height : 300,
+		 		 		border : true,
+		 		 		title : '둘째 패널'
+		 		 	}
+		 		]
+		 	}
+		]
+	})
+	//layout : 'fit, 'border'
+/*	Ext.create("Ext.container.Viewport",{
 		renderTo : Ext.getBody(),
 		layout : 'border',
 		border : true,
@@ -33,6 +69,6 @@ Ext.onReady(function(){
 				region : 'center',
 			}
 		]
-	});
+	});*/
 });
 
